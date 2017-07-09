57fa59c517dcbecbf64b547bfcb18ba5225ac105
amansawhney <amansawhney@sawhneygames.com>
2017-07-09 18:47:54 -0400

    added animate.css

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ function myMove() {
     var elem = document.getElementById("myAnimation");
     var pos = 0;
     var id = setInterval(frame, 2);
+    elem.classList.add("fadeOut");
     function frame() {
         if (pos == -50) {
             clearInterval(id);
@@ -12,4 +13,4 @@ function myMove() {
     }
 }
 
-myMove()
+myMove();
