ff336a6001a6d843ad97d67808e71bfa4440ed6a
amansawhney <amansawhney@sawhneygames.com>
2017-07-09 18:58:47 -0400

    made element fade out afteraward

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 function myMove() {
     var elem = document.getElementById("myAnimation");
     var pos = 0;
-    var id = setInterval(frame, 2);
-    elem.classList.add("fadeOut");
+    var id = setInterval(frame, 1);
     function frame() {
         if (pos == -50) {
             clearInterval(id);
+            elem.className += " animated fadeOut"
         } else {
             pos--;
             elem.style.top = pos + 'px';
+
         }
     }
 }
